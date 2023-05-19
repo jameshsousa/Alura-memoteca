fa828834f54a55376310093e3bb42e54c41e5253
James Holanda <jameshsousa@gmail.com>
2023-05-19 09:55:25 -0300

    incluir, excluir e editar pensamentos

--- src/app/components/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/components/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Pensamento } from '../pensamento';
+import { PensamentoService } from '../pensamento.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-criar-pensamento',
@@ -8,18 +10,29 @@ import { Pensamento } from '../pensamento';
 })
 export class CriarPensamentoComponent {
   pensamento: Pensamento = {
-    id: 1,
-    conteudo: "Aprendendo Angular",
-    autoria: "Dev",
+
+    conteudo: "",
+    autoria: "",
     modelo: "modelo1"
 
   }
 
-  criarPensamento() {
-    alert("Novo Pensamento Criado")
+  constructor(
+    private service: PensamentoService,
+    private router: Router
+  ){ }
+
+  ngOninit(): void {
+
   }
 
-  cancelarPensamento() {
-    alert("Pensamento cancelado com sucesso!")
+  criarPensamento() {
+    this.service.criar(this.pensamento).subscribe(() => {
+      this.router.navigate(['/listarPensamento'])
+    });
+  }
+
+  cancelar() {
+    this.router.navigate(['/listarPensamento'])
   }
 }
